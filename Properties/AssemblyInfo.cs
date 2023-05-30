a3b474c09302fc91a96a41585f0cf230b4799400
Kagamine Haku <blomaway@gmail.com>
2023-05-31 02:28:59 +0700

    LastStable

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MikuRetailPro")]
-[assembly: AssemblyDescription("Special thanks to Mr.Xuyen helped me complete this software\n PhamGiaHuy\n 2108110026")]
+[assembly: AssemblyDescription("Special thanks to Mr.Xuyen helped me complete this software PhamGiaHuy 2108110026")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("KagamineHaku")]
 [assembly: AssemblyProduct("MikuRetailPro")]
@@ -34,3 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
